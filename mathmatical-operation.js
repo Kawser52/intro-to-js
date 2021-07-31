0b610bd44c76166a676350e631e2b08796d484b7
Soriful Islam Kawser <shoriful521399@gmail.com>
2021-08-01 00:37:39 +0600

    Matha complete

---
+++ mathmatical-operation.js
@@ -0,0 +1,7 @@
+var kochurDam=53;
+var morchiDam=45;
+var kachaBajar=kochurDam+morchiDam;
+var diffreneceKachabajar=morchiDam-kochurDam;
+console.log(kachaBajar);
+console.log(diffreneceKachabajar);
+
