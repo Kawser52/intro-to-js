e869e99aac5f8e64488ede57fd288165a56787c8
Soriful Islam Kawser <shoriful521399@gmail.com>
2021-08-01 09:02:07 +0600

    Math complete

---
+++ math-operation.js
@@ -0,0 +1,15 @@
+var Price=10;
+var ringChipsPrice=10;
+Price+=1;
+Price++;
+ringChipsPrice--;
+ringChipsPrice-=1;
+
+var sum=Price+ringChipsPrice;
+
+console.log(Price);
+console.log(sum);
+console.log(ringChipsPrice);
+
+
+
