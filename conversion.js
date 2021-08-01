76ac61b11572f72dc056ca03e762c8bf46f62abd
Soriful Islam Kawser <shoriful521399@gmail.com>
2021-08-01 09:36:36 +0600

    Conversion Complte

---
+++ conversion.js
@@ -0,0 +1,12 @@
+var maleName="Abudl";
+var femaleName="Jorina";
+
+var fullName=maleName + ' ' + femaleName;
+// console.log(fullName);
+
+var morchiPrice= '45';
+var adaPrice='25.5';
+var totalPrice=morchiPrice + ' ' + adaPrice;
+var morchierDam=parseInt(morchiPrice);
+var adarDam=parseFloat(adaPrice);
+console.log(morchierDam+adarDam);
